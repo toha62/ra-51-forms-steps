362ec10f96a985c196595f3c42de4647c920f9f1
Anton Trifonov <toha62@inbox.ru>
2023-04-18 12:50:38 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Listing from './components/Listing';
+import StepForm from './components/StepForm';
+
+
+function App() {
+  return (
+    <>
+      <Listing items={items} /> 
+      <StepForm />  
+    </>    
+  )
+}
+
+export default App
